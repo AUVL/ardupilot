b781e6afba04b75da8f9f0ac14f31a1fb89832bc
unknown <nx@Austin.(none)>
2016-06-08 17:31:11 +0800

    libraries/AP_GPS/AP_GPS_NMEA.cpp

--- libraries/AP_GPS/AP_GPS_NMEA.cpp
+++ libraries/AP_GPS/AP_GPS_NMEA.cpp
@@ -307,7 +307,7 @@ bool AP_GPS_NMEA::_term_complete()
                     make_gps_time(_new_date, _new_time * 10);
                     state.last_gps_time_ms = now;
                     // To-Do: add support for proper reporting of 2D and 3D fix
-                    state.status           = AP_GPS::GPS_OK_FIX_3D;
+			//state.status           = AP_GPS::GPS_OK_FIX_3D;			//austin
                     fill_3d_velocity();
                     break;
                 case _GPS_SENTENCE_GGA:
@@ -318,7 +318,18 @@ bool AP_GPS_NMEA::_term_complete()
                     state.num_sats      = _new_satellite_count;
                     state.hdop          = _new_hdop;
                     // To-Do: add support for proper reporting of 2D and 3D fix
-                    state.status        = AP_GPS::GPS_OK_FIX_3D;
+                    if(_gps_data_FixRTK){
+                    state.status	= AP_GPS::GPS_OK_FIX_3D_RTK;//austin
+                    }
+			else if(_gps_data_FloatRTK){
+			state.status	= AP_GPS::GPS_OK_FIX_3D_DGPS;//austin
+			}
+			else
+			{
+			state.status           = AP_GPS::GPS_OK_FIX_3D;		
+			}
+			//state.status        = AP_GPS::GPS_OK_FIX_3D_DGPS;
+			break;
                     break;
                 case _GPS_SENTENCE_VTG:
                     _last_VTG_ms = now;
@@ -382,6 +393,8 @@ bool AP_GPS_NMEA::_term_complete()
             break;
         case _GPS_SENTENCE_GGA + 6: // Fix data (GGA)
             _gps_data_good = _term[0] > '0';
+	     _gps_data_FixRTK = _term[0] =='4'; 		// read gps status in GGA sentence 4 for FixRTK		//austin
+	     _gps_data_FloatRTK = _term[0] =='5'; 		// read gps status in GGA sentence 5 for FloatRTK	//austin
             break;
         case _GPS_SENTENCE_VTG + 9: // validity (VTG) (we may not see this field)
             _gps_data_good = _term[0] != 'N';
