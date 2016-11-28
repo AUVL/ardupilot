217fff275903679ed1f7cad15d5c8b3376de888c
unknown <admin@ASUS780.(none)>
2016-11-28 21:23:33 +0800

    foobar

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -39,7 +39,7 @@ void Copter::userhook_MediumLoop()
 void Copter::userhook_SlowLoop()
 {
     // put your 3.3Hz code here
-    int RX[10];
+    int RX;
 	RX= hal.uartE->read();
 	hal.console->printf("%s",RX);
 }
