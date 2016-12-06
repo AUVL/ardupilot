6ed31a575eefdabedc2637b59a3c89720f2b3eb3
unknown <admin@ASUS780.(none)>
2016-12-06 14:04:36 +0800

    foobar

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -42,5 +42,28 @@ void Copter::userhook_SlowLoop()
 void Copter::userhook_SuperSlowLoop()
 {
     // put your 1Hz code here
+    uint32_t fix_time;
+    // if we have a GPS fix, take the time as the last fix time. That
+    // allows us to correctly calculate velocities and extrapolate
+    // positions.
+    // If we don't have a GPS fix then we are dead reckoning, and will
+    // use the current boot time as the fix time.
+    if (gps.status() >= AP_GPS::GPS_OK_FIX_2D) {
+        fix_time = gps.last_fix_time_ms();
+    } else {
+        fix_time = millis();
+    }
+    const Vector3f &vel = inertial_nav.get_velocity();
+    mavlink_msg_global_position_int_send(
+        MAVLINK_COMM_2,
+        fix_time,
+        current_loc.lat,                // in 1E7 degrees
+        current_loc.lng,                // in 1E7 degrees
+        (ahrs.get_home().alt + current_loc.alt) * 10UL,      // millimeters above sea level
+        current_loc.alt * 10,           // millimeters above ground
+        vel.x,                          // X speed cm/s (+ve North)
+        vel.y,                          // Y speed cm/s (+ve East)
+        vel.z,                          // Z speed cm/s (+ve up)
+        ahrs.yaw_sensor);               // compass heading in 1/100 degree
 }
 #endif
