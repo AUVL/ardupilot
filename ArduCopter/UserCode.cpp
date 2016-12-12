958b11cb99f51851f44beaba66b5999f00d94418
unknown <admin@ASUS780.(none)>
2016-12-12 16:37:06 +0800

    foobar

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -1,7 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 #include "Copter.h"
-#include "Follow_target.h"
 
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
